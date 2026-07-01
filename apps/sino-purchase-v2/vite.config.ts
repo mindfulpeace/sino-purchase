171a18e92ea47275fec80b2fffd8deae884fe2cf
mindfulpeace <ren.jintao@icloud.com>
2026-07-01 19:49:34 +0200

    chore: 修改 base path 为根路径 /

--- apps/sino-purchase-v2/vite.config.ts
+++ apps/sino-purchase-v2/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/sino-purchase/v2/desk/",
+  base: "/",
   plugins: [react()],
   build: {
     target: "es2020",
